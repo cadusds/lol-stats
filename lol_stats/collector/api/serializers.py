b6babd3a93b563d52210f25b73c157e878e99bab
cadusds <carlos.silva150995@gmail.com>
2023-04-20 03:07:11 -0300

    add coluna id no serializer

--- lol_stats/collector/api/serializers.py
+++ lol_stats/collector/api/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class SummonersSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.Summoners
-        fields = ['summoner_id', 'account_id', 'name', 'profile_icon_id', 'revision_date', 'summoner_level']
+        fields = ['id','summoner_id', 'account_id', 'name', 'profile_icon_id', 'revision_date', 'summoner_level']
