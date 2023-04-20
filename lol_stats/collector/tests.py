5dfd97d3334fbeada22f099c620df18a76b20e5f
cadusds <carlos.silva150995@gmail.com>
2023-04-20 00:56:20 -0300

    adicionando teste para o model de summoners

--- lol_stats/collector/tests.py
+++ lol_stats/collector/tests.py
@@ -1,3 +1,24 @@
+import uuid
+import datetime
 from django.test import TestCase
+from collector import models
 
-# Create your tests here.
+
+class SummonersTestCase(TestCase):
+
+    def setUp(self) -> None:
+        models.Summoners.objects.create(
+            summoner_id = uuid.uuid4(),
+            account_id = uuid.uuid4(),
+            puuid = uuid.uuid4(),
+            name = "TestSummoner",
+            profile_icon_id = uuid.uuid4(),
+            revision_date = datetime.datetime(2022,1,1),
+            summoner_level = 101
+        )
+    
+    def test_create_info(self):
+        summoner = models.Summoners.objects.get(name="TestSummoner")
+        self.assertEqual(summoner.name,"TestSummoner")
+        self.assertEqual(summoner.revision_date, datetime.datetime(2022,1,1,tzinfo=datetime.timezone.utc))
+        self.assertEqual(summoner.summoner_level,101)
