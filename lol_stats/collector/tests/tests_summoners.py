296c7c73c27edeebc8ec9f6effba0750ef185ee6
cadusds <carlos.silva150995@gmail.com>
2023-04-20 23:37:07 -0300

    update

--- lol_stats/collector/tests/tests_summoners.py
+++ lol_stats/collector/tests/tests_summoners.py
@@ -6,7 +6,6 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-
 class SummonersTestCase(TestCase):
 
     def setUp(self) -> None:
@@ -28,8 +27,8 @@ class SummonersTestCase(TestCase):
 
 class SummonersAPITestCase(APITestCase):
 
-    def test_create_summoner(self):
-        data = dict(
+    def setUp(self) -> None:
+        self.summoner_data = dict(
             summoner_id = uuid.uuid4(),
             account_id = uuid.uuid4(),
             puuid = uuid.uuid4(),
@@ -38,8 +37,15 @@ class SummonersAPITestCase(APITestCase):
             revision_date = datetime.datetime(2022,1,1),
             summoner_level = 101
         )
+
+    def test_create_summoner(self):
         url = reverse("summoner-list")
-        response = self.client.post(url,data,format="json")
+        response = self.client.post(url,self.data,format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(models.Summoners.objects.count(),1)
         self.assertEqual(models.Summoners.objects.get().name,"TestSummoner")
+    
+    def test_retrieve_summoner(self):
+        summoner = models.Summoners.objects.create(**self.summoner_data)
+        response = self.client.get(reverse('summoner-retrieve',kwargs={"pk":summoner.pk}))
+        self.assertEqual(response.data,self.summoner_data)
