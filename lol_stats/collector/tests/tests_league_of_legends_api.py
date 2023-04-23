6a69221b2222689d46d72d733a8f23ac5de6c3c2
cadusds <carlos.silva150995@gmail.com>
2023-04-23 20:11:19 -0300

    improve tesrts

--- lol_stats/collector/tests/tests_league_of_legends_api.py
+++ lol_stats/collector/tests/tests_league_of_legends_api.py
@@ -1,25 +1,33 @@
+import uuid
+import json
 import requests
+import datetime
 from django.test import TestCase
 from unittest.mock import MagicMock
 from collector.api.league_of_legends_api import LeagueOfLegendsAPI
+
 
 
 class LeagueOfLegendsAPITestCase(TestCase):
 
     def setUp(self) -> None:
         self.lol_api = LeagueOfLegendsAPI()
+        self.summonner_data = self.build_summoner_data()
+    
+    def build_summoner_data(self):
+        return dict(
+            id = str(uuid.uuid4()),
+            accountId = str(uuid.uuid4()),
+            puuid = str(uuid.uuid4()),
+            name = "xxxx",
+            profileIconId = str(uuid.uuid4()),
+            revisionDate = datetime.datetime.timestamp(datetime.datetime.utcnow())*100,
+            summonerLevel = 101
+        )
     
     def build_summoner_response(self):
         response = requests.Response()
-        response._content = """{
-            "id": "xxx-xxx",
-            "accountId": "xxxxx",
-            "puuid": "xxx-xxx",
-            "name": "xxxx",
-            "profileIconId": 4378,
-            "revisionDate": 1682179506000,
-            "summonerLevel": 116
-        }""".encode('utf-8')
+        response._content = json.dumps(self.summonner_data).encode('utf-8')
         return response
     
     def test_get_summoner(self):
@@ -28,3 +36,4 @@ class LeagueOfLegendsAPITestCase(TestCase):
         response = self.lol_api.get_summoner("xxxx")
         self.assertIsInstance(response,dict)
         requests.get.assert_called_with("https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/xxxx", headers=self.lol_api.headers)
+        self.assertEqual(response["revision_date"],datetime.datetime.fromtimestamp(self.summonner_data["revisionDate"]/100))
