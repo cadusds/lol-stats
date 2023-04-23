cb6b92b95c29535ede8b93f9abef632336c76d17
cadusds <carlos.silva150995@gmail.com>
2023-04-23 20:10:13 -0300

    refac create method

--- lol_stats/collector/api/views.py
+++ lol_stats/collector/api/views.py
@@ -1,13 +1,16 @@
 from collector import models
-from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework import viewsets, status, permissions
 from collector.api import serializers
+from rest_framework.response import Response
 
 
 class SummonerViewSet(viewsets.ModelViewSet):
-    queryset = models.Summoners.objects.all()
-    serializer_class = serializers.SummonersSerializer
+    queryset = models.Summoner.objects.all()
+    serializer_class = serializers.SummonerSerializer
     permission_classes = [permissions.AllowAny]
 
-    def create(self, request, *args, **kwargs):
-        return super().create(request,*args,**kwargs)
+    def create(self, request):
+        summoner_name = request.data["name"]
+        summoner = models.Summoner.objects.create(summoner_name)
+        summoner = serializers.SummonerSerializer(summoner)
+        return Response(summoner.data,status=status.HTTP_201_CREATED)
