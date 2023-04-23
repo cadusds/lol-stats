dc4bf727452d0e4e2138bbddda7d712f82fe1021
cadusds <carlos.silva150995@gmail.com>
2023-04-23 20:09:54 -0300

    change summoner name

--- lol_stats/collector/api/serializers.py
+++ lol_stats/collector/api/serializers.py
@@ -2,7 +2,7 @@ from collector import models
 from rest_framework import serializers
 
 
-class SummonersSerializer(serializers.HyperlinkedModelSerializer):
+class SummonerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = models.Summoners
+        model = models.Summoner
         fields = ['id','summoner_id', 'account_id', 'name', 'profile_icon_id', 'revision_date', 'summoner_level']
