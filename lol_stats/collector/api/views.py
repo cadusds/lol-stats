469a1d4559aab0ce53fa9a6052eae4e2c5db5884
cadusds <carlos.silva150995@gmail.com>
2023-04-27 02:38:01 -0300

    add restricao para nomes repetidos

--- lol_stats/collector/api/views.py
+++ lol_stats/collector/api/views.py
@@ -2,6 +2,7 @@ from collector import models
 from rest_framework import viewsets, status, permissions
 from collector.api import serializers
 from rest_framework.response import Response
+from django.db.utils import IntegrityError
 
 
 class SummonerViewSet(viewsets.ModelViewSet):
@@ -11,6 +12,9 @@ class SummonerViewSet(viewsets.ModelViewSet):
 
     def create(self, request):
         summoner_name = request.data["name"]
-        summoner = models.Summoner.objects.create(summoner_name)
+        try:
+            summoner = models.Summoner.objects.create(summoner_name)
+        except IntegrityError:
+            return Response({"error":f"The summoner {summoner_name} already exists."}, status=status.HTTP_400_BAD_REQUEST)
         summoner = serializers.SummonerSerializer(summoner)
         return Response(summoner.data,status=status.HTTP_201_CREATED)
--- lol_stats/collector/tests/tests_summoners.py
+++ lol_stats/collector/tests/tests_summoners.py
@@ -7,24 +7,25 @@ from rest_framework import status
 from unittest.mock import MagicMock
 from collector.models import Summoner
 from rest_framework.test import APITestCase
+from collector.tests.generate_data import GenarateData
 from collector.api.serializers import SummonerSerializer
 from collector.api.league_of_legends_api import LeagueOfLegendsAPI
 
 
-def build_summoner_data():
+def build_summoner_data(summoner_name:str):
     return dict(
         id = str(uuid.uuid4()),
         accountId = str(uuid.uuid4()),
         puuid = str(uuid.uuid4()),
-        name = "xxxx",
+        name = summoner_name,
         profileIconId = str(uuid.uuid4()),
         revisionDate = 1682179506000,
         summonerLevel = 101
     )
 
-def build_summoner_response():
+def build_summoner_response(summoner_name:str):
     response = requests.Response()
-    response._content = json.dumps(build_summoner_data()).encode('utf-8')
+    response._content = json.dumps(build_summoner_data(summoner_name)).encode('utf-8')
     return response
 
 class SummonerTestCase(TestCase):
@@ -34,38 +35,56 @@ class SummonerTestCase(TestCase):
     
     def test_create_summoner(self):
         requests.get = MagicMock()
-        requests.get.return_value = build_summoner_response()
-        summoner = Summoner.objects.create("xxxx")
-        requests.get.assert_called_with("https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/xxxx", headers=self.lol_api.headers)
-        self.assertEqual(summoner.name, "xxxx")
+        summoner_name = GenarateData.get_random_string(7)
+        requests.get.return_value = build_summoner_response(summoner_name)
+        summoner = Summoner.objects.create(summoner_name)
+        requests.get.assert_called_with(f"https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner_name}", headers=self.lol_api.headers)
+        self.assertEqual(summoner.name, summoner_name)
 
 class SummonerAPITestCase(APITestCase):
 
     def setUp(self) -> None:
         self.maxDiff = None
-
+    
     def create_summoner(self):
         requests.get = MagicMock()
-        requests.get.return_value = build_summoner_response()
-        return Summoner.objects.create("xxxx")
+        summoner_name = GenarateData.get_random_string(7)
+        requests.get.return_value = build_summoner_response(summoner_name)
+        return Summoner.objects.create(summoner_name)
 
     def test_create_summoner(self):
         url = reverse("summoner-list")
-        response = self.client.post(url,{"name":"xxxx"})
+        summoner_name = GenarateData.get_random_string(7)
+        requests.get = MagicMock()
+        requests.get.return_value = build_summoner_response(summoner_name)
+        response = self.client.post(url,{"name":summoner_name})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Summoner.objects.count(),1)
         summoner = Summoner.objects.first()
-        self.assertEqual(summoner.name,"xxxx")
+        self.assertEqual(summoner.name,summoner_name)
+    
+    def test_create_summoner_already_exists(self):
+        summoner_name = GenarateData.get_random_string(7)
+        Summoner.objects.create(summoner_name=summoner_name)    
+        url = reverse("summoner-list")
+        response = self.client.post(url,{"name":summoner_name})
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        expected_data = dict(
+            error=f"The summoner {summoner_name} already exists."
+        )
+        self.assertEqual(response.data, expected_data)
     
     def test_retrieve_summoner(self):
-        summoner = Summoner.objects.create("xxxx")
+        summoner_name = GenarateData.get_random_string(7)
+        summoner = Summoner.objects.create(summoner_name)
         response = self.client.get(reverse('summoner-detail',kwargs={"pk":summoner.pk}))
         summoner = SummonerSerializer(summoner)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data,summoner.data)
     
     def test_delete_summoner(self):
-        summoner = Summoner.objects.create("xxxx")
+        summoner_name = GenarateData.get_random_string(7)
+        summoner = Summoner.objects.create(summoner_name)
         response = self.client.delete(reverse('summoner-detail',kwargs=dict(pk=summoner.pk)))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Summoner.objects.count(),0)
