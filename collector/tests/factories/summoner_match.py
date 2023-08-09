79455ca1e88443a933ed31b20d364cf04465f639
cadusds <carlos.silva150995@gmail.com>
2023-08-09 18:49:35 -0300

    create a factory

---
+++ collector/tests/factories/summoner_match.py
@@ -0,0 +1,19 @@
+import factory
+import random
+from collector import models
+from .summoner import SummonerFactory
+from collector.tests.generate_data import GenerateData
+
+
+def generate_match_id() -> str:
+    options = GenerateData._build_match_response_data()
+    return random.choice(options)    
+
+class SummonerMatchFactory(factory.django.DjangoModelFactory):
+    
+    class Meta:
+        model = models.SummonerMatch
+    
+    summoner = factory.SubFactory(SummonerFactory)
+    match_id = factory.LazyFunction(generate_match_id)
+    game_id = factory.LazyAttribute(lambda x: x.match_id.replace("BR1_",""))
