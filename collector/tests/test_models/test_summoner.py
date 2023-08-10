a51e231d4536244eeef6bcdd646ff1f72874e8fa
cadusds <carlos.silva150995@gmail.com>
2023-08-10 00:57:49 -0300

    refatorando testes

--- collector/tests/test_models/test_summoner.py
+++ collector/tests/test_models/test_summoner.py
@@ -7,19 +7,17 @@ from collector.api.league_of_legends_api import LeagueOfLegendsAPI
 from faker import Faker
 from ..factories.summoner import SummonerFactory
 
-fake = Faker()
-fake_name = fake.name()
 
 class SummonerTestCase(TestCase):
     def setUp(self) -> None:
         self.lol_api = LeagueOfLegendsAPI()
         self.factory = SummonerFactory
 
-    @patch.object( requests, "get", return_value=GenerateData.build_lol_api_summoner_response(fake_name))
-    def test_create_summoner(self,mocked):
-        summoner = self.factory.create(name=fake_name)
+    @patch.object( requests, "get", return_value=GenerateData.build_lol_api_summoner_response("test"))
+    def test_create_summoner_by_name(self,mocked):
+        summoner = Summoner.objects.create_summoner_by_name(name="test")
         requests.get.assert_called_with(
-            f"https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{fake_name}",
+            f"https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/test",
             headers=self.lol_api.headers,
         )
-        self.assertEqual(summoner.name, fake_name)
+        self.assertEqual(summoner.name, "test")
