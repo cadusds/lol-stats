2b86158ba43bdcb80057339086b05038be94c64a
cadusds <carlos.silva150995@gmail.com>
2023-08-10 14:55:13 -0300

    criando a factory dos dados basicos da partida

--- collector/tests/factories/match_participant_basic_stats.py
+++ collector/tests/factories/match_participant_basic_stats.py
@@ -2,25 +2,27 @@ import factory
 import factory.fuzzy
 from collector import models
 from collector.tests.generate_data import GenerateData
+from .summoner import SummonerFactory
+from .match import MatchFactory
 
 
 class MatchParticipantBasicStatsFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.MatchParticipantBasicStats
 
-    # summoner = models.ForeignKey(Summoner, on_delete=models.CASCADE, null=False)
-    # game_id = models.ForeignKey(SummonerMatch, on_delete=models.CASCADE, null=False)
-    # team_position = models.CharField(max_length=200)
-    # deaths = models.IntegerField()
-    # assists = models.IntegerField()
-    # kills = models.IntegerField()
-    # double_kills = models.IntegerField()
-    # triple_kills = models.IntegerField()
-    # quadra_kills = models.IntegerField()
-    # penta_kills = models.IntegerField()
-    # first_blood_kill = models.BooleanField()
-    # first_blood_assist = models.BooleanField()
-    # first_tower_kill = models.BooleanField()
-    # first_tower_assist = models.BooleanField()
-    # largest_multi_kill = models.IntegerField()
-    # win = models.BooleanField()
+    summoner = factory.SubFactory(SummonerFactory)
+    game = factory.SubFactory(MatchFactory)
+    team_position = factory.fuzzy.FuzzyText(length=10)
+    deaths = factory.fuzzy.FuzzyInteger(low=0,high=15)
+    assists = factory.fuzzy.FuzzyInteger(low=0,high=15)
+    kills = factory.fuzzy.FuzzyInteger(low=0,high=15)
+    double_kills = factory.fuzzy.FuzzyInteger(low=0,high=10)
+    triple_kills = factory.fuzzy.FuzzyInteger(low=0,high=6)
+    quadra_kills = factory.fuzzy.FuzzyInteger(low=0,high=3)
+    penta_kills = factory.fuzzy.FuzzyInteger(low=0,high=2)
+    first_blood_kill = factory.fuzzy.FuzzyChoice([True,False])
+    first_blood_assist = factory.fuzzy.FuzzyChoice([True,False])
+    first_tower_kill = factory.fuzzy.FuzzyChoice([True,False])
+    first_tower_assist = factory.fuzzy.FuzzyChoice([True,False])
+    largest_multi_kill = factory.fuzzy.FuzzyInteger(low=0,high=10)
+    win = factory.fuzzy.FuzzyChoice([True,False])
