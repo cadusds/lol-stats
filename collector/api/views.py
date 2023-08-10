1e29439114f31d95d5515e0dd966f2f53fbb7977
cadusds <carlos.silva150995@gmail.com>
2023-08-10 00:56:53 -0300

    acertos após mudança de nome

--- collector/api/views.py
+++ collector/api/views.py
@@ -14,7 +14,7 @@ class SummonerViewSet(viewsets.ModelViewSet):
     def create(self, request):
         summoner_name = request.data["name"]
         try:
-            summoner = models.Summoner.objects.create(summoner_name)
+            summoner = models.Summoner.objects.create_summoner_by_name(summoner_name)
         except IntegrityError:
             return Response(
                 {"error": f"The summoner {summoner_name} already exists."},
