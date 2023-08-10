75a833d2fbc4240a79b9fee2893341af9b319e82
cadusds <carlos.silva150995@gmail.com>
2023-08-10 03:40:26 -0300

    refac match tests

--- collector/tests/factories/summoner_match.py
+++ collector/tests/factories/summoner_match.py
@@ -1,13 +1,9 @@
 import factory
+import factory.fuzzy
 import random
 from collector import models
 from .summoner import SummonerFactory
 from collector.tests.generate_data import GenerateData
-
-
-def generate_match_id() -> str:
-    options = GenerateData._build_match_response_data()
-    return random.choice(options)
 
 
 class SummonerMatchFactory(factory.django.DjangoModelFactory):
@@ -15,5 +11,5 @@ class SummonerMatchFactory(factory.django.DjangoModelFactory):
         model = models.SummonerMatch
 
     summoner = factory.SubFactory(SummonerFactory)
-    match_id = factory.LazyFunction(generate_match_id)
+    match_id = factory.fuzzy.FuzzyChoice(GenerateData._build_match_response_data())
     game_id = factory.LazyAttribute(lambda x: x.match_id.replace("BR1_", ""))
